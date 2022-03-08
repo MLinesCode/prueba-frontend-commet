b63d91dea47f3fe221c757af8d4759377a63e6b8
Miguel Garcia <mikexx1264@gmail.com>
2022-03-07 22:01:34 -0600

    Estado incial de la aplicacion

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	extends: [
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'airbnb',
+		'eslint-config-prettier',
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {
+		'react/react-in-jsx-scope': 'off',
+	},
+};
