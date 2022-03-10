f22fc7633827178b036423e0cc85eafd749f8088
Miguel Garcia <mikexx1264@gmail.com>
2022-03-10 15:58:49 -0600

    Se agrega la llamada a la API

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import CardUser from './components/carduser/CardUser';
 import Header from './layout/Header';
 
 function App() {
+	const [user, setUser] = useState([]);
+	useEffect(() => {
+		fetch('https://dummyapi.io/data/v1/user?limit=10', {
+			method: 'GET',
+			headers: {
+				'app-id': '6227d862779227583bf7885d',
+				'Content-Type': 'application/json',
+			},
+		})
+			.then(response => response.json())
+			.then(data => setUser(data.data));
+	}, []);
+
 	return (
-		<div>
+		<main>
 			<Header />
-			<CardUser />
-		</div>
+			<div>
+				{user.map(user => (
+					<CardUser
+						key={user.id}
+						picture={user.picture}
+						firstName={user.firstName}
+						id={user.id}
+					/>
+				))}
+			</div>
+		</main>
 	);
 }
 
