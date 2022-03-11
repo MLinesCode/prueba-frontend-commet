1fe819f730ecb5245454b089312a113b68d04f30
Miguel Garcia <mikexx1264@gmail.com>
2022-03-11 17:36:09 -0600

    Se crea el componente PostItem y los estilos

---
+++ src/components/postitem/PostItem.jsx
@@ -0,0 +1,18 @@
+import './postitem.css';
+
+function PostItem() {
+	return (
+		<div className='post-container'>
+			<h3 className='post-title'>Titulo del post</h3>
+			<h4 className='post-date'>fecha del post</h4>
+			<p className='post-content'>
+				Lorem ipsum dolor sit amet consectetur elit. Perferendis ab porro ea
+				provident voluptate doloribus dolorum voluptatem? Sit, aut. Sapiente
+				iste autem maxime repellendus tenetur harum dolorem architecto ea
+				voluptas.
+			</p>
+		</div>
+	);
+}
+
+export default PostItem;
