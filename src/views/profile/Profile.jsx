5a82bbc61432fa443b30930b9dd26453c3192a6c
Miguel Garcia <mikexx1264@gmail.com>
2022-03-11 17:37:00 -0600

    Se crea la vista Profile y se agregan los componentes a la misma

---
+++ src/views/profile/Profile.jsx
@@ -0,0 +1,37 @@
+import { Link } from 'react-router-dom';
+import PostItem from '../../components/postitem/PostItem';
+import UserInfo from '../../components/userinfo/UserInfo';
+import IconBack from '../../assets/back.png';
+import './profile.css';
+
+function Profile() {
+	return (
+		<article className='profile-background'>
+			<Link to='/'>
+				<div className='profile-navigation'>
+					<img src={IconBack} alt={IconBack} />
+				</div>
+			</Link>
+			<section className='profile-user'>
+				<div className='profile-info'>
+					<UserInfo
+						picture='https://randomuser.me/api/portraits/'
+						firstName='Sara'
+						lastName='Andersen'
+						registerDate='12 de octubre de 2019'
+					/>
+				</div>
+			</section>
+			<section className='profile-posts'>
+				<h2>Mis posts</h2>
+				<PostItem />
+				<PostItem />
+				<PostItem />
+				<PostItem />
+				<PostItem />
+			</section>
+		</article>
+	);
+}
+
+export default Profile;
