6f42a3e859bc787f45600e29a7605cbbfbe0a555
Miguel Garcia <mikexx1264@gmail.com>
2022-03-11 03:44:09 -0600

    Rutas agregadas - (/) Home (/profile) Profile

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import './index.css';
 import App from './App';
+import Profile from './views/profile/Profile';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<App />} />
+				<Route path='/profile' element={<Profile />} />
+			</Routes>
+		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
