2a8ed229634cb1b421cb61881a05da59ef2a0419
Miguel Garcia <mikexx1264@gmail.com>
2022-03-11 17:35:39 -0600

    Se creo el componete UserInfo y sus estilos

---
+++ src/components/userinfo/UserInfo.jsx
@@ -0,0 +1,17 @@
+import './userinfo.css';
+
+// eslint-disable-next-line react/prop-types
+function UserInfo({ picture, firstName, lastName, registerDate }) {
+	return (
+		<div className='profile-container'>
+			<figure className='profile-image'>
+				<img src={picture} alt={firstName} />
+			</figure>
+			<h2 className='profile-name'>{firstName}</h2>
+			<h3 className='profile-lastname'>{lastName}</h3>
+			<p className='profile-register'>{registerDate}</p>
+		</div>
+	);
+}
+
+export default UserInfo;
